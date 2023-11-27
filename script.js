b4739f9e1cf0d1055fd595edb55ecc26e67edad9
gardrh <gardrh@gmail.com>
2023-11-27 13:51:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function toggleMenu() {
+    console.log('Toggle menu function called');
     var nav = document.querySelector('nav ul');
     nav.classList.toggle('show');
 }
+
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    var nav = document.querySelector('nav ul');
+    nav.classList.toggle('show');
+});
